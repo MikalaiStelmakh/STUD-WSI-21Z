7814c773f6a4f4d115ea2e979c93f0ac0091b881
Mikalai Stelmakh <01165876@pw.edu.pl>
2021-10-25 18:58:27 +0200

    Add sidebar to graphs

--- lab1/benchmark_chart.py
+++ lab1/benchmark_chart.py
@@ -10,20 +10,40 @@ def readFromJson(path):
 
 
 if __name__ == "__main__":
-    data = readFromJson(".benchmarks/data.json")["steepest_descent_method"]
+    METHOD = "steepest_descent_method"
+    # METHOD = "newton_method"
+    data = readFromJson(".benchmarks/data.json")[METHOD]
 
     fig, ax = plt.subplots()
-    Xs = list(set([benchmark["point"][0] for benchmark in data["benchmarks"]]))
-    Ys = list(set([benchmark["point"][1] for benchmark in data["benchmarks"]]))
+    Xs = [benchmark["point"][0] for benchmark in data["benchmarks"]]
+    Ys = [benchmark["point"][1] for benchmark in data["benchmarks"]]
+
+    def remove_duplicates(seq):
+        seen = set()
+        seen_add = seen.add
+        return [x for x in seq if not (x in seen or seen_add(x))]
+    Xs = remove_duplicates(Xs)
+    Ys = remove_duplicates(Ys)
     iterations = [benchmark["iterations"] for benchmark in data["benchmarks"]]
     width = len(Xs)
 
     def chunks(l, size):
         return [l[i:i+size] for i in range(0, len(l), size)]
 
+    if METHOD == "steepest_descent_method":
+        for index, _ in enumerate(iterations):
+            iterations[index] /= 10000
+            iterations[index] = round(iterations[index], 1)
     iterations = np.array(chunks(iterations, width))
 
     im = ax.imshow(iterations)
+    cbar = ax.figure.colorbar(im, ax=ax)
+    if METHOD == "steepest_descent_method":
+        cbar.ax.set_ylabel("Iterations x 10000", rotation=-90, va="bottom")
+    else:
+        cbar.ax.set_ylabel("Iterations", rotation=-90, va="bottom")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
     ax.set_xticks(np.arange(len(Xs)))
     ax.set_yticks(np.arange(len(Ys)))
     ax.set_xticklabels(Xs)
@@ -35,4 +55,4 @@ if __name__ == "__main__":
                            ha="center", va="center", color="w")
     ax.set_title("Iterations")
     fig.tight_layout()
-    plt.show()
+    plt.savefig("graphs/" + "iterations_" + METHOD)
