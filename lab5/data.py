5d67bfffac91857ff485eefdb0200cbe8e47c37b
Mikalai Stelmakh <01165876@pw.edu.pl>
2021-12-30 19:39:19 +0100

    Modify images to be matrix instead of vector

--- lab5/data.py
+++ lab5/data.py
@@ -6,6 +6,6 @@ def get_mnist():
     with np.load(f"{pathlib.Path(__file__).parent.absolute()}/data/mnist.npz") as f:
         images, labels = f["x_train"], f["y_train"]
     images = images.astype("float32") / 255
-    images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
+    images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2], 1))
     labels = np.eye(10)[labels]
     return images, labels
